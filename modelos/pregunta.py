7ff74ae9fee661eb67d87617af4fc43ef29d9192
Jonathan N. Lubo <30604250+JoniBe@users.noreply.github.com>
2024-11-28 23:44:58 -0300

    fix: mejor en el init de una pregunta

--- modelos/pregunta.py
+++ modelos/pregunta.py
@@ -3,7 +3,7 @@ class Pregunta:
     def __init__(self, pregunta: str, opciones: list[str], respuesta_correcta: int):
         self.pregunta = pregunta
         self.opciones = opciones
-        self.respuesta_correcta = respuesta_correcta
+        self.respuesta_correcta = int(respuesta_correcta)
 
     def validar_respuesta(self, respuesta: int) -> bool:
         return respuesta == self.respuesta_correcta
