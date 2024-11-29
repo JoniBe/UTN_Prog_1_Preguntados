19e2b8326be383d5f98fc8e81b24d273462fcecb
Jonathan N. Lubo <30604250+JoniBe@users.noreply.github.com>
2024-11-28 23:45:56 -0300

    feat: clean code y mejoras de funcionalidad

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 import pygame
 from modelos.ventana import Ventana
 from modelos.menu_principal import MenuPrincipal
-from modelos.boton import Boton
-from constantes import VENTANA_MENU_PRINCIPAL, VENTANA_SALIR, FPS
+from Constantes import VENTANA_MENU_PRINCIPAL, VENTANA_SALIR, VENTANA_JUGAR, VENTANA_FIN_PARTIDA, FPS, CANTIDAD_VIDAS
 from funciones.componentes import crear_lista_botones
+from fin_de_partida import terminar_juego
+from modelos.partida import Partida
 
-ventana_actual = VENTANA_MENU_PRINCIPAL
-
-corriendo = True
+pygame.mixer.init()
+pygame.mixer.music.load("assets/cancion_juego.mp3")
 
 ventana = Ventana().renderizar()
 
@@ -17,17 +17,35 @@ menu_principal = MenuPrincipal(lista_botones)
 
 reloj = pygame.time.Clock()
 
-while corriendo:
-    reloj.tick(FPS)
+corriendo = True
 
+ventana_actual = VENTANA_MENU_PRINCIPAL
+
+datos_juego = {
+    "puntuacion": 0,
+    "vidas": CANTIDAD_VIDAS,
+    "usuario": "",
+    "acertados_seguidos": 1,
+    "tiempo": 60
+}
+
+partida = Partida(ventana)
+
+while corriendo:
     cola_eventos = pygame.event.get()
 
     if ventana_actual == VENTANA_MENU_PRINCIPAL:
         ventana_actual = menu_principal.ejecutar(ventana, cola_eventos)
+    elif ventana_actual == VENTANA_JUGAR:
+        ventana_actual = partida.ejecutar(cola_eventos)
+    elif ventana_actual == VENTANA_FIN_PARTIDA:
+        ventana_actual = terminar_juego(ventana, cola_eventos, datos_juego)
     elif ventana_actual == VENTANA_SALIR:
         corriendo = False
 
     pygame.display.flip()
+    
+    reloj.tick(FPS)
 
 # Finalizar el juego
 pygame.quit()
