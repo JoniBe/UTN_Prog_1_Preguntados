85690f9f72bd8ed32b01e319839328c0179d9697
Jonathan N. Lubo <30604250+JoniBe@users.noreply.github.com>
2024-11-30 11:50:26 -0300

    feat: limpiar clase Ventana

--- modelos/ventana.py
+++ modelos/ventana.py
@@ -1,17 +1,13 @@
 import pygame
-from Constantes import VENTANA_MEDIDA, BACKGROUND_MENU_PRINCIPAL
+from Constantes import VENTANA_MEDIDA
 
 # Ventana
 class Ventana:
     def __init__(self):
         self.caption = 'PREGUNTADOS'
-        self.medida = VENTANA_MEDIDA
-        self.background_menu_principal = pygame.transform.scale(BACKGROUND_MENU_PRINCIPAL, VENTANA_MEDIDA)
 
     def renderizar(self) -> pygame.Surface:
         pygame.display.set_caption(self.caption)
-        ventana = pygame.display.set_mode(self.medida)
-        background = pygame.transform.scale(self.background_menu_principal, self.medida)
-        ventana.blit(background, (0, 0))
+        ventana = pygame.display.set_mode(VENTANA_MEDIDA)
 
         return ventana
