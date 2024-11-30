6f2c822bf00c8d766e67ced28165e60ac9b1cae6
Jonathan N. Lubo <30604250+JoniBe@users.noreply.github.com>
2024-11-30 11:49:44 -0300

    add: nueva clase para Juego

---
+++ modelos/juego.py
@@ -0,0 +1,46 @@
+import pygame
+from modelos.ventana import Ventana
+from modelos.partida import Partida
+from modelos.partida_finalizada import PartidaFinalizada
+from modelos.menu_principal import MenuPrincipal
+from Constantes import *
+
+# Juego
+class Juego:
+    def __init__(self):
+        self.ventana_actual = VENTANA_MENU_PRINCIPAL
+        self.ventana = Ventana().renderizar()
+        self.menu_principal = MenuPrincipal(self.ventana)
+        self.partida = Partida(self.ventana)
+        self.partida_finalizada = PartidaFinalizada(self.ventana)
+        self.reloj = pygame.time.Clock()
+        self.corriendo = True
+
+    def ejecutar_musica(self):
+        pygame.mixer.init()
+        pygame.mixer.music.load(MUSICA_JUEGO_PATH)
+        pygame.mixer.music.play(-1)
+
+    def ejecutar(self):
+        self.ejecutar_musica()
+
+        while self.corriendo:
+            cola_eventos = pygame.event.get()
+
+            if self.ventana_actual == VENTANA_MENU_PRINCIPAL:
+                self.ventana_actual = self.menu_principal.ejecutar(cola_eventos, self.ventana_actual)
+            elif self.ventana_actual == VENTANA_JUGAR:
+                self.ventana_actual = self.partida.jugar(cola_eventos, self.ventana_actual)
+            elif self.ventana_actual == VENTANA_PARTIDA_FINALIZADA:
+                self.ventana_actual = self.partida_finalizada.mostrar(cola_eventos, self.ventana_actual, self.partida)
+            elif self.ventana_actual == VENTANA_SALIR:
+                self.corriendo = False
+
+            pygame.display.flip()
+
+            self.reloj.tick(FPS)
+
+        self.finalizar()
+    
+    def finalizar(self):
+        pygame.quit()
