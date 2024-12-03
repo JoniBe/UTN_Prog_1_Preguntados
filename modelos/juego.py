7fc91b98ceae7c1a0d9c055aaf111ec1e3325b75
Jonathan N. Lubo <30604250+JoniBe@users.noreply.github.com>
2024-12-03 15:44:53 -0300

    feat: agregar acceso a configuracion

--- modelos/juego.py
+++ modelos/juego.py
@@ -1,10 +1,11 @@
 import pygame
+from constantes import *
 from modelos.ventana import Ventana
 from modelos.partida import Partida
 from modelos.partida_finalizada import PartidaFinalizada
 from modelos.menu_principal import MenuPrincipal
 from modelos.ranking import Ranking
-from constantes import *
+from modelos.configuracion import Configuracion
 
 # Juego
 class Juego:
@@ -15,6 +16,7 @@ class Juego:
         self.partida = Partida(self.ventana)
         self.partida_finalizada = PartidaFinalizada(self.ventana)
         self.ranking = Ranking(self.ventana)
+        self.configuracion = Configuracion(self.ventana)
         self.reloj = pygame.time.Clock()
         self.corriendo = True
 
@@ -35,10 +37,12 @@ class Juego:
                 self.ventana_actual = self.partida.jugar(cola_eventos, self.ventana_actual)
             elif self.ventana_actual == VENTANA_PARTIDA_FINALIZADA:
                 self.ventana_actual = self.partida_finalizada.mostrar(cola_eventos, self.ventana_actual, self.partida)
-            elif self.ventana_actual == VENTANA_SALIR:
-                self.corriendo = False
             elif self.ventana_actual == VENTANA_RANKING:
                 self.ventana_actual = self.ranking.mostrar(cola_eventos, self.ventana_actual)
+            elif self.ventana_actual == VENTANA_CONFIGURACION:
+                self.ventana_actual = self.configuracion.mostrar(cola_eventos, self.ventana_actual)
+            elif self.ventana_actual == VENTANA_SALIR:
+                self.corriendo = False
 
             pygame.display.flip()
 
