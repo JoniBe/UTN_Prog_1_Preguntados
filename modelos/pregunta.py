da673cd5866e493bcf2a7b156ae7e497f5a5a94c
Jonathan N. Lubo <30604250+JoniBe@users.noreply.github.com>
2024-12-03 14:06:36 -0300

    add: nueva clase pregunta

--- modelos/pregunta.py
+++ modelos/pregunta.py
@@ -1,9 +1,12 @@
+from modelos.respuesta import Respuesta
+
 # Pregunta
 class Pregunta:
-    def __init__(self, pregunta: str, opciones: list[str], respuesta_correcta: int):
+    def __init__(self, pregunta: str, respuestas: list[Respuesta], respuesta_correcta: int):
         self.pregunta = pregunta
-        self.opciones = opciones
+        self.respuestas = respuestas
         self.respuesta_correcta = int(respuesta_correcta)
 
-    def validar_respuesta(self, respuesta: int) -> bool:
-        return respuesta == self.respuesta_correcta
+    def validar_respuesta(self, respuesta: str) -> bool:
+        respuesta_correcta_texto = self.respuestas[self.respuesta_correcta - 1].respuesta
+        return respuesta.upper() == respuesta_correcta_texto.upper()
